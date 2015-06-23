a3806ffb56042123d239db38dfe69a116f163023
Volker89 <volker-heidemann@gmx.de>
2015-06-23 13:54:41 +0200

    Schrift Final

--- app/src/main/java/voluda/treasurehuntkali/FinalActivity.java
+++ app/src/main/java/voluda/treasurehuntkali/FinalActivity.java
@@ -1,5 +1,7 @@
 package voluda.treasurehuntkali;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +15,36 @@ public class FinalActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
+        TextView myTextview = (TextView) findViewById(R.id.textView10);
+        myTextview.setTypeface(myTypeface);
+        myTextview.setTextColor(Color.parseColor("#fbda9d"));
+        myTextview.setShadowLayer(8, 0, 0, Color.BLACK);
+
+        Typeface myTypeface2 = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
+        TextView myTextview2 = (TextView) findViewById(R.id.textView11);
+        myTextview2.setTypeface(myTypeface2);
+        myTextview2.setTextColor(Color.parseColor("#fbda9d"));
+        myTextview2.setShadowLayer(8, 0, 0, Color.BLACK);
+
+        Typeface myTypeface3 = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
+        TextView myTextview3 = (TextView) findViewById(R.id.textView12);
+        myTextview3.setTypeface(myTypeface3);
+        myTextview3.setTextColor(Color.parseColor("#fbda9d"));
+        myTextview3.setShadowLayer(8, 0, 0, Color.BLACK);
+
+        Typeface myTypeface4 = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
+        TextView myTextview4 = (TextView) findViewById(R.id.textView13);
+        myTextview4.setTypeface(myTypeface4);
+        myTextview4.setTextColor(Color.parseColor("#fbda9d"));
+        myTextview4.setShadowLayer(8, 0, 0, Color.BLACK);
+
+        Typeface myTypeface5 = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
+        TextView myTextview5 = (TextView) findViewById(R.id.textView14);
+        myTextview5.setTypeface(myTypeface5);
+        myTextview5.setTextColor(Color.parseColor("#fbda9d"));
+        myTextview5.setShadowLayer(8, 0, 0, Color.BLACK);
 
         Globals g = Globals.getInstance();
         int data = g.getData();
