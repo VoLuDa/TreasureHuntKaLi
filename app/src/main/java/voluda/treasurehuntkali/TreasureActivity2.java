9f131304ba8ddaa3be9961056022404322f66ef9
Volker89 <volker-heidemann@gmx.de>
2015-06-23 14:01:14 +0200

    Schrift Aufgabe 1 - 4

--- app/src/main/java/voluda/treasurehuntkali/TreasureActivity2.java
+++ app/src/main/java/voluda/treasurehuntkali/TreasureActivity2.java
@@ -1,6 +1,8 @@
 package voluda.treasurehuntkali;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 
 public class TreasureActivity2 extends ActionBarActivity {
@@ -17,6 +20,12 @@ public class TreasureActivity2 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treasure_activity2);
+
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
+        TextView myTextview = (TextView) findViewById(R.id.textView5);
+        myTextview.setTypeface(myTypeface);
+        myTextview.setTextColor(Color.parseColor("#fbda9d"));
+        myTextview.setShadowLayer(8, 0, 0, Color.BLACK);
 
         RadioButton eins = (RadioButton)findViewById(R.id.radioButton);
         RadioButton zwei = (RadioButton)findViewById(R.id.radioButton2);
