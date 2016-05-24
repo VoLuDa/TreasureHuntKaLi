84a9585ab27a10f359a6ed3ce73f67ba356d75dd
Volker89 <volker-heidemann@gmx.de>
2016-05-24 14:15:54 +0200

    Remove Title Bar in Main und Menue

--- app/src/main/java/voluda/treasurehuntkali/MainActivity.java
+++ app/src/main/java/voluda/treasurehuntkali/MainActivity.java
@@ -1,6 +1,7 @@
 package voluda.treasurehuntkali;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -9,14 +10,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
--- app/src/main/java/voluda/treasurehuntkali/MenueActivity.java
+++ app/src/main/java/voluda/treasurehuntkali/MenueActivity.java
@@ -1,5 +1,6 @@
 package voluda.treasurehuntkali;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -8,14 +9,16 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 
-public class MenueActivity extends ActionBarActivity {
+public class MenueActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_menue);
 
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Windlass.ttf");
